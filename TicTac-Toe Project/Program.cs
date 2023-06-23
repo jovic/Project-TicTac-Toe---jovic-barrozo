7811c11b505cf069c730e62ef7d71633e6ea1953
jovic <jOviC45@yahoo.com>
2023-06-22 22:44:57 -0600

    Test Board Class

--- TicTac-Toe Project/Program.cs
+++ TicTac-Toe Project/Program.cs
@@ -1,67 +1,96 @@
 ﻿using System;
 using System.Collections.Generic;
-using TicTac_Toe_Project;
+using TicTac_Toe_Project.FourGame;
 
 namespace TicTac_Toe_Project
 {
-    internal class Program
+    public class FourGame
     {
         public string Name { get; set; }
         public int Score { get; set; }
-        private static List<GuessingGame> _playersList;
+        private static List<FourGame> _playersList;
         private static int count;
 
-
-        public class Controller
+        static FourGame()
         {
-            _playersList = new List<GuessingGame>();
+            _playersList = new List<FourGame>();
             count = 0;
         }
-
-        public class Controller(string playerName)
+        public FourGame()
         {
-            Name = playerName;
+
+        }
+
+        public FourGame(string name)
+        {
+            Name = name;
             Score = 0;
         }
 
-    public override string ToString()
-    {
-        return $"Name: {Name}, Score: {Score}.";
-    }
-    abstract class Player
-    {
-
-    }
-    public class HumanPlayer
-    {
-
-    }
-
-    public class ComupterPlayer
-    {
-
-    }
-
-    //public class Board
-    //{
-    //    public Board(int width, int height)
-    //    {
-    //        for (int i = 0; i < height; i++)
-    //        {
-    //            Console.Write("|\t");
-    //            for (int j = 0; j < width; j++)
-    //            {
-    //                Console.WriteLine("j\t");
-    //            }
-    //            Console.WriteLine("|");
-    //        }
-    //    }
-               
-    //}
-    static void Main(string[] args)
+        public override string ToString()
         {
-            Console.WriteLine("Hello World!\t");
-            //var boardObj = new Board(7, 6);
+            return $"Name: {Name}, Score: {Score}.";
+        }
+
+
+        abstract class Player
+        {
+
+        }
+        public class HumanPlayer
+        {
+
+        }
+
+        public class ComupterPlayer
+        {
+
+        }
+
+        public class Board
+        {
+            public Board()
+            {
+
+            }
+            public Board(int width, int height)
+            {
+                for (int i = 0; i < height; i++)
+                {
+                    Console.Write("| ");
+                    for (int j = 0; j < width; j++)
+                    {
+                        Console.Write($"# ");
+                    }
+                    Console.WriteLine("|");
+                }
+                for (int i = 0; i < 1; i++)
+                {
+                    Console.Write("  ");
+                    for (int j = 1; j <= width; j++)
+                    {
+                        Console.Write($"{j} ");
+                    }
+                    Console.WriteLine(" ");
+                }
+            }
+
+        }
+    }
+
+
+
+
+    internal class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nConnect 4 Game Development Project:\n");
+            var boardObj = new Board(7, 6);
+
         }
     }
 }
